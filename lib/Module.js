razvandobrovat/webpack

remove the need to rewrite chunk in reasons

lib/Module.js
@@ -10,6 +10,7 @@ const DependenciesBlock = require("./DependenciesBlock");
 const ModuleReason = require("./ModuleReason");
 const SortableSet = require("./util/SortableSet");
 const Template = require("./Template");
+const ChunkGroup = require("./ChunkGroup");
 
 const EMPTY_RESOLVE_OPTIONS = {};
 
@@ -68,9 +69,6 @@ class Module extends DependenciesBlock {
 		this.used = null;
 		this.usedExports = null;
 		this.optimizationBailout = [];
-
-		// delayed operations
-		this._rewriteChunkInReasons = undefined;
 	}
 
 	get exportsArgument() {
@@ -86,7 +84,6 @@ class Module extends DependenciesBlock {
 		this.renderedHash = undefined;
 
 		this.reasons.length = 0;
-		this._rewriteChunkInReasons = undefined;
 		this._chunks.clear();
 
 		this.id = null;
@@ -197,37 +194,52 @@ class Module extends DependenciesBlock {
 		return false;
 	}
 
-	hasReasonForChunk(chunk) {
-		if(this._rewriteChunkInReasons) {
-			for(const operation of this._rewriteChunkInReasons)
-				this._doRewriteChunkInReasons(operation.oldChunk, operation.newChunks);
-			this._rewriteChunkInReasons = undefined;
+	isAccessibleInChunk(chunk, ignoreChunk) {
+		// Check if module is accessible in ALL chunk groups
+		for(const chunkGroup of chunk.groupsIterable) {
+			if(!this.isAccessibleInChunkGroup(chunkGroup))
+				return false;
 		}
-		for(let i = 0; i < this.reasons.length; i++) {
-			if(this.reasons[i].hasChunk(chunk))
-				return true;
+		return true;
+	}
+
+	isAccessibleInChunkGroup(chunkGroup, ignoreChunk) {
+		const queue = new Set([chunkGroup]);
+
+		// Check if module is accessible from all items of the queue
+		queueFor: for(const cg of queue) {
+			// 1. If module is in one of the chunks of the group we can continue checking the next items
+			//    because it's accessible.
+			for(const chunk of cg.chunks) {
+				if(chunk !== ignoreChunk && chunk.containsModule(this))
+					continue queueFor;
+			}
+			// 2. If the chunk group is initial, we can break here because it's not accessible.
+			if(chunkGroup.isInitial())
+				return false;
+			// 3. Enqueue all parents because it must be accessible from ALL parents
+			for(const parent of chunkGroup.parentsIterable)
+				queue.add(parent);
+		}
+		// When we processed through the whole list and we didn't bailout, the module is accessible
+		return true;
+	}
+
+	hasReasonForChunk(chunk) {
+		// check for each reason if we need the chunk
+		for(const reason of this.reasons) {
+			const fromModule = reason.module;
+			for(const originChunk of fromModule.chunksIterable) {
+				// return true if module this is not reachable from originChunk when ignoring cunk
+				if(!this.isAccessibleInChunk(originChunk, chunk))
+					return true;
+			}
 		}
 		return false;
 	}
 
 	hasReasons() {
 		return this.reasons.length > 0;
-	}
-
-	rewriteChunkInReasons(oldChunk, newChunks) {
-		// This is expensive. Delay operation until we really need the data
-		if(this._rewriteChunkInReasons === undefined)
-			this._rewriteChunkInReasons = [];
-		this._rewriteChunkInReasons.push({
-			oldChunk,
-			newChunks
-		});
-	}
-
-	_doRewriteChunkInReasons(oldChunk, newChunks) {
-		for(let i = 0; i < this.reasons.length; i++) {
-			this.reasons[i].rewriteChunks(oldChunk, newChunks);
-		}
 	}
 
 	isUsed(exportName) {

youzaiyouzai666/webpack-1

lib/Module.js
@@ -64,9 +64,6 @@ class Module extends DependenciesBlock {
 		this.used = null;
 		this.usedExports = null;
 		this.optimizationBailout = [];
-
-		// delayed operations
-		this._rewriteChunkInReasons = undefined;
 	}
 
 	get exportsArgument() {
@@ -82,7 +79,6 @@ class Module extends DependenciesBlock {
 		this.renderedHash = undefined;
 
 		this.reasons.length = 0;
-		this._rewriteChunkInReasons = undefined;
 		this._chunks.clear();
 
 		this.id = null;
@@ -187,36 +183,48 @@ class Module extends DependenciesBlock {
 		return false;
 	}
 
-	hasReasonForChunk(chunk) {
-		if (this._rewriteChunkInReasons) {
-			for (const operation of this._rewriteChunkInReasons)
-				this._doRewriteChunkInReasons(operation.oldChunk, operation.newChunks);
-			this._rewriteChunkInReasons = undefined;
+	isAccessibleInChunk(chunk, ignoreChunk) {
+		// Check if module is accessible in ALL chunk groups
+		for (const chunkGroup of chunk.groupsIterable) {
+			if (!this.isAccessibleInChunkGroup(chunkGroup)) return false;
 		}
-		for (let i = 0; i < this.reasons.length; i++) {
-			if (this.reasons[i].hasChunk(chunk)) return true;
+		return true;
+	}
+
+	isAccessibleInChunkGroup(chunkGroup, ignoreChunk) {
+		const queue = new Set([chunkGroup]);
+
+		// Check if module is accessible from all items of the queue
+		queueFor: for (const cg of queue) {
+			// 1. If module is in one of the chunks of the group we can continue checking the next items
+			//    because it's accessible.
+			for (const chunk of cg.chunks) {
+				if (chunk !== ignoreChunk && chunk.containsModule(this))
+					continue queueFor;
+			}
+			// 2. If the chunk group is initial, we can break here because it's not accessible.
+			if (chunkGroup.isInitial()) return false;
+			// 3. Enqueue all parents because it must be accessible from ALL parents
+			for (const parent of chunkGroup.parentsIterable) queue.add(parent);
+		}
+		// When we processed through the whole list and we didn't bailout, the module is accessible
+		return true;
+	}
+
+	hasReasonForChunk(chunk) {
+		// check for each reason if we need the chunk
+		for (const reason of this.reasons) {
+			const fromModule = reason.module;
+			for (const originChunk of fromModule.chunksIterable) {
+				// return true if module this is not reachable from originChunk when ignoring cunk
+				if (!this.isAccessibleInChunk(originChunk, chunk)) return true;
+			}
 		}
 		return false;
 	}
 
 	hasReasons() {
 		return this.reasons.length > 0;
-	}
-
-	rewriteChunkInReasons(oldChunk, newChunks) {
-		// This is expensive. Delay operation until we really need the data
-		if (this._rewriteChunkInReasons === undefined)
-			this._rewriteChunkInReasons = [];
-		this._rewriteChunkInReasons.push({
-			oldChunk,
-			newChunks
-		});
-	}
-
-	_doRewriteChunkInReasons(oldChunk, newChunks) {
-		for (let i = 0; i < this.reasons.length; i++) {
-			this.reasons[i].rewriteChunks(oldChunk, newChunks);
-		}
 	}
 
 	isUsed(exportName) {